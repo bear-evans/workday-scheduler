cc4e2a712d53e3955c7d80ce0b7c46c0fad8f888
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-05-26 21:40:32 -0400

    Added all time rows, created time functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,63 @@
+/// ==================================
+// Date and Time Module
+// -----------------------------------
+// Handles all Moment.js calls
+// ===================================
 var dateAndTime = (function() {
     
-});
+    function getDate() {
+        let date = moment().format('MMMM Do YYYY');
+        return date;
+    }
+
+    function getHour() {
+        let hour = moment().format('HH');
+        return hour;
+    }
+
+    return {
+        getDate : getDate,
+        getHour : getHour
+    }
+})();
+
+/// ==================================
+// Planner Module
+// -----------------------------------
+// Handles DOM manipulation and user
+// interaction
+// ===================================
+var plannerApp = (function() {
+
+    let headerDate = document.getElementById("currentDay");
+
+    function updateDate () {
+        headerDate.textContent = dateAndTime.getDate();
+    }
+
+    function init() {
+        updateDate();
+    }
+
+    return {
+        init: init
+    }
+
+})();
+
+/// ==================================
+// Storage Module
+// -----------------------------------
+// Handles retrieving and storing data
+// in local storage
+// ===================================
+var storeManager = (function() {
+
+})();
+
+/// ==================================
+// Runtime Code
+// -----------------------------------
+// Runs on load
+// ===================================
+plannerApp.init();
